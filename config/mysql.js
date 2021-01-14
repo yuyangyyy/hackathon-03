daa3bb31c9df64b57f9c18a5d9723fa1619a22b5
Grégory <grg.vilar@gmail.com>
2021-01-14 12:10:41 +0100

    recupere back

--- config/mysql.js
+++ config/mysql.js
@@ -5,6 +5,7 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
