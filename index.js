27ba6ec4c93f9c2ce3715b359c547b5c036d0219
Romain heloise <romheloise@gmail.com>
2021-01-18 10:21:01 +0100

    ready for deploy

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const produits = require('./routes/produits');
-const categories = require('./routes/categories');
-const images = require('./routes/images');
-const panier = require('./routes/panier');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const produits = require("./routes/produits");
+const categories = require("./routes/categories");
+const images = require("./routes/images");
+const panier = require("./routes/panier");
+const path = require("path");
 
 const app = express();
 
@@ -12,10 +13,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/produits', produits);
-app.use('/categories', categories);
-app.use('/uploads', images);
-app.use('/paniers', panier);
+app.use("/produits", produits);
+app.use("/categories", categories);
+app.use("/uploads", images);
+app.use("/paniers", panier);
+
+app.use("/", express.static(path.resolve(__dirname, "./public/build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "build", "index.html"));
+});
 
 const server = app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server is listening on : ${process.env.SERVER_PORT}`);
